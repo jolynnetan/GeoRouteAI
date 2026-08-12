4c9b92d2d41ed90768dfeacfd79632c05938f826
Tan Xiao Shih <xiaoshih123@gmail.com>
2026-08-12 19:13:18 +0800

    Update dispatcher workflow and UI

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,12 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // <-- Fix dot to slash here!
-import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    tsconfigPaths: true,
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
